c465bd766161c05170c0c69d0e159784a360ac56
Erika Miguel-Yeo <erika@bocoup.com>
2019-07-15 17:29:18 -0400

    breaking out init asserts

--- tests/sb-util.test.ts
+++ tests/sb-util.test.ts
@@ -19,13 +19,17 @@ test('ScratchProject not initialized on multiple present options', () => {
 	}
 });
 
-test('ScratchProject intialized', async () => {
+test('ScratchProject intialized with sb3 file', async () => {
 	// Test init on sb3 file
 	await expect(initialize({ 'file': `${process.cwd()}/tests/data/test.sb3` })).resolves.toBeInstanceOf(ScratchProject);
-	
+});
+
+test('ScratchProject intialized with project.json file', async () => {
 	// Test init on project.json
 	await expect(initialize({ 'file': `${process.cwd()}/tests/data/project.json`})).resolves.toBeInstanceOf(ScratchProject);
+});
 
+test('ScratchProject intialized with cloud ID', async () => {
 	await expect(initialize({ 'cloudId': 319383115 })).resolves.toBeInstanceOf(ScratchProject);
 });
 
