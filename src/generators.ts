e3f96b7592394dec4b30712bed43d94ab3ced6de
Mx. Corey Frang <gnarf37@gmail.com>
2019-08-16 14:55:00 -0400

    Add 'Iterable' versions of the Iterator methods in generator

--- src/generators.ts
+++ src/generators.ts
@@ -1,13 +1,29 @@
+export function makeIterable<T, U>(iter: T, predicate: (item: T) => Iterator<U>): Iterable<U> {
+    return {
+        [Symbol.iterator](): Iterator<U> {
+            return predicate(iter);
+        },
+    };
+}
+
 export function* map<T, O>(iter: Iterable<T>, predicate: (item: T) => O): Iterator<O> {
     for (const item of iter) {
         yield predicate(item);
     }
 }
 
+export function mapIterable<T, O>(iter: Iterable<T>, predicate: (item: T) => O): Iterable<O> {
+    return makeIterable(null, (): Iterator<O> => map(iter, predicate));
+}
+
 export function* flatmap<T, O>(iter: Iterable<T>, predicate: (item: T) => Iterable<O>): Iterator<O> {
     for (const item of iter) {
         yield* predicate(item);
     }
+}
+
+export function flatmapIterable<T, O>(iter: Iterable<T>, predicate: (item: T) => Iterable<O>): Iterable<O> {
+    return makeIterable(null, (): Iterator<O> => flatmap(iter, predicate));
 }
 
 export function* filter<T>(iter: Iterable<T>, predicate: (item: T) => boolean): Iterator<T> {
@@ -18,12 +34,8 @@ export function* filter<T>(iter: Iterable<T>, predicate: (item: T) => boolean): 
     }
 }
 
-export function makeIterable<T, U>(iter: T, predicate: (item: T) => Iterator<U>): Iterable<U> {
-    return {
-        [Symbol.iterator](): Iterator<U> {
-            return predicate(iter);
-        },
-    };
+export function filterIterable<T>(iter: Iterable<T>, predicate: (item: T) => boolean): Iterable<T> {
+    return makeIterable(null, (): Iterator<T> => filter(iter, predicate));
 }
 
 export function chain<T>(...iterables: Iterable<T>[]): Iterable<T> {
