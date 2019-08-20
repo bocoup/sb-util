fbdac506bbbbd3cc34849703d2a91c765509cdb9
Mx. Corey Frang <gnarf37@gmail.com>
2019-08-20 11:51:32 -0400

    Add docblocks to base

--- src/base.ts
+++ src/base.ts
@@ -10,10 +10,17 @@ export class PropertiesWrapper<T> {
         propsStorage.set(this, props);
     }
 
+    /**
+     * Return the raw Properties objects
+     */
     public props(): T {
         return propsStorage.get(this);
     }
 
+    /**
+     * Read a property from the stored properties object
+     * @param property The name of the property key
+     */
     // prop can be a string, number, object, or boolean
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public prop(property: string): any {
@@ -21,22 +28,35 @@ export class PropertiesWrapper<T> {
     }
 }
 
+/**
+ * Generic Collection wrapper
+ */
 export class CollectionWrapper<T, U> {
+    // Assign to the type of the PropertiesWrapper that we create
     protected static PropClass: new (T) => unknown = PropertiesWrapper;
 
     public constructor(props: Iterable<T>) {
         propsStorage.set(this, props);
     }
 
+    /**
+     * Return the raw iterable that returns raw properties instead of wrapped properties.
+     */
     public propsIterable(): Iterable<T> {
         return propsStorage.get(this);
     }
 
+    /**
+     * Iterate the collection mapping to a utility class for additional query.
+     */
     public [Symbol.iterator](): Iterator<U> {
-        const PropClass = (this.constructor as typeof CollectionWrapper).PropClass;
+        const { PropClass } = this.constructor as typeof CollectionWrapper;
         return map(this.propsIterable(), (item: T): U => new PropClass(item) as U);
     }
 
+    /**
+     * Return first item in collection.
+     */
     public first(): U {
         return first(this);
     }
